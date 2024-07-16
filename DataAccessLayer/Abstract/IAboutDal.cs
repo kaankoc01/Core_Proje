583037aef754ec47c20307afe09f94caa1b1eac0
kaan koc <kaankoc01@gmail.com>
2024-07-17 01:44:36 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IAboutDal.cs
@@ -0,0 +1,13 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IAboutDal:IGenericDal<About>
+    {
+ 
+    }
+}
