b50aa0aadfeeca5d6fb3beefe95f2209174598b8
kaan koc <kaankoc01@gmail.com>
2024-08-02 00:59:59 +0300

    First Commit

---
+++ Core_Proje/Controllers/SkillController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager SkillManager = new SkillManager(new EfSkillDal());
+        public IActionResult Index()
+        {
+            var values = SkillManager.TGetList();
+            return View(values);
+        }
+    }
+}
